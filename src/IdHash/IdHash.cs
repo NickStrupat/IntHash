75ae1d89ab8f9840a4f633deec0e69d51265280e
Nick Strupat <nick.strupat@gmail.com>
2025-04-05 22:31:08 -0400

    Initial code commit with some unit tests

---
+++ src/IdHash/IdHash.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NickStrupat;
+
+/// <summary>
+/// A pair of 32-bit hash functions with no collisions on all 32-bit inputs
+/// </summary>
+/// <note>Ported from https://github.com/skeeto/hash-prospector/blob/396dbe235c94dfc2e9b559fc965bcfda8b6a122c/README.md?plain=1#L237</note>
+public static class IdHash
+{
+	/// A perfect hash function for 32-bit integers. No collisions for all 32-bit values. The inverse of <see cref="Hash2"/>.
+	/// <param name="x">The integer to hash</param>
+	/// <returns>The hashed integer</returns>
+	public static UInt32 Hash(UInt32 x)
+	{
+		unchecked
+		{
+			x += 2;
+			x ^= x >> 17;
+			x *= 0xed5ad4bb;
+			x ^= x >> 11;
+			x *= 0xac4c1b51;
+			x ^= x >> 15;
+			x *= 0x31848bab;
+			x ^= x >> 14;
+			return x;
+		}
+	}
+
+	/// A perfect hash function for 32-bit integers. No collisions for all 32-bit values. The inverse of <see cref="Hash"/>.
+	/// <param name="x">The integer to hash</param>
+	/// <returns>The hashed integer</returns>
+	public static UInt32 HashInverse(UInt32 x)
+	{
+		unchecked
+		{
+			x ^= x >> 14 ^ x >> 28;
+			x *= 0x32b21703;
+			x ^= x >> 15 ^ x >> 30;
+			x *= 0x469e0db1;
+			x ^= x >> 11 ^ x >> 22;
+			x *= 0x79a85073;
+			x ^= x >> 17;
+			x -= 2;
+			return x;
+		}
+	}
+}
---
+++ test/UnitTests/UnitTest1.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using NickStrupat;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace UnitTests;
+
+public class IdHashTests(ITestOutputHelper toh)
+{
+	// private Action<String> print = toh
+	// 	.GetType()
+	// 	.GetMethod("QueueTestOutput", BindingFlags.NonPublic | BindingFlags.Instance)
+	// 	?.CreateDelegate<Action<String>>()!;
+	
+	[Fact]
+	public void Hash_WhenCalledWithZero_DoesNotReturnZero()
+	{
+		var result = IdHash.Hash(0);
+		Assert.NotEqual(0u, result);
+	}
+	
+	[Fact]
+	public void HashInverse_WhenCalledWithZero_DoesNotReturnZero()
+	{
+		var result = IdHash.HashInverse(0);
+		Assert.NotEqual(0u, result);
+	}
+	
+	[Fact]
+	public void HashInverse_WhenCalledWithTheResultOfCallingHash_ReturnsTheOriginalValue()
+	{
+		const UInt32 percent = UInt32.MaxValue / 100;
+		for (var i = 0ul; i <= UInt32.MaxValue; i++)
+		{
+			var (quotient, remainder) = Math.DivRem(i, percent);
+			if (remainder == 0)
+				toh.WriteLine(quotient + "%");
+			
+			var input = (UInt32)i;
+			var hash = IdHash.Hash(input);
+			if (input == hash) // Assert class methods are too slow
+				Assert.Fail("Input and hash result are equal: " + input + " => " + hash);
+			var inverse = IdHash.HashInverse(hash);
+			if (input != inverse) // Assert class methods are too slow
+				Assert.Fail("Input and inverse result are not equal: " + input + " => " + hash + " => " + inverse);
+		}
+	}
+	
+	[Fact]
+	public void Hash_WhenCalledWithAllPossibleValues_ReturnsUniqueValues()
+	{
+		var _32BitValueCount = UInt32.MaxValue + 1ul;
+		var bitStore = new Byte[_32BitValueCount / 8];
+		const UInt32 tenPercent = UInt32.MaxValue / 10;
+		for (var i = 0ul; i <= UInt32.MaxValue; i++)
+		{
+			var (quotient, remainder) = Math.DivRem(i, tenPercent);
+			if (remainder == 0)
+				toh.WriteLine(quotient + "%");
+			
+			var input = (UInt32)i;
+			var hash = IdHash.Hash(input);
+			var byteIndex = (Int32)(hash / 8);
+			var bitIndex = (Byte)(hash & 7);
+			ref var @byte = ref bitStore[byteIndex];
+			var bit = (@byte >> bitIndex) & 1;
+			if (bit == 1)
+			{
+				Assert.Fail($"Collision found at {i} => {hash}");
+			}
+			else
+			{
+				@byte |= (Byte)(1 << bitIndex);
+			}
+		}
+		toh.WriteLine("No collision found");
+		
+		Assert.True(bitStore.All(x => x == 0xff), "Collision found");
+	}
+}
+
+static class ITestOutputHelperExtensions
+{
+	public static void Write(this ITestOutputHelper toh, String message)
+	{
+		ArgumentNullException.ThrowIfNull(toh);
+		if (toh is not TestOutputHelper x)
+			throw new ArgumentException("This `ITestOutputHelper` is not an instance of the `TestOutputHelper` class.");
+		ArgumentNullException.ThrowIfNull(message);
+		var print = printDelegate.GetValue(toh, t => queueTestOutput.CreateDelegate<Action<String>>(t));
+		print(message);
+	}
+	
+	private static readonly ConditionalWeakTable<ITestOutputHelper, Action<String>> printDelegate = new();
+
+	private static readonly MethodInfo queueTestOutput = typeof(TestOutputHelper)
+		.GetMethod("QueueTestOutput", BindingFlags.NonPublic | BindingFlags.Instance)
+		?? throw new InvalidOperationException("Could not find the QueueTestOutput method.");
+}
